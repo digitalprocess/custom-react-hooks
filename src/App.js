06fcdbb8afd41e760777fc445c5e123c0273abcd
frontendista <csdelreal@gmail.com>
2020-04-07 01:17:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Card, CardGrid, Container, Header } from "./Elements";
+import "./App.css";
+import Menu from "./Menu";
+import blue from "./blue.png";
+import purp from "./purp.png";
+import black from "./black.png";
+import green from "./green.png";
+
+import Toggle from './components/Toggle'
+
+function App() {
+	return (
+		<div>
+			<Header>
+				<Menu />
+				<h1>Header</h1>
+			</Header>
+			<Container>
+				<h2>Super Cool</h2>
+				<Toggle />
+				<CardGrid>
+					<Card style={{ background: "var(--purp)" }}>
+						<h3>Some card</h3>
+						<img src={purp} alt="purp" />
+					</Card>
+					<Card style={{ background: "var(--blue)" }}>
+						<h3>Some card</h3>
+						<img src={blue} alt="blue" />
+					</Card>
+					<Card style={{ background: "var(--black)" }}>
+						<h3>Some card</h3>
+						<img src={black} alt="black" />
+					</Card>
+					<Card style={{ background: "var(--green)" }}>
+						<h3>Some card</h3>
+						<img src={green} alt="green" />
+					</Card>
+				</CardGrid>
+			</Container>
+		</div>
+	);
+}
+
+export default App;
