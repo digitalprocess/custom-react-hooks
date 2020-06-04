13be33a81bd21582f4fb3a3753ff9cfa6f9c8e2b
frontendista <csdelreal@gmail.com>
2020-06-04 15:56:36 -0500

    React Context & Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,53 +1,60 @@
-import React from "react";
-import { Card, CardGrid, Container, Header } from "./Elements";
-import "./App.css";
-import Menu from "./Menu";
-import blue from "./blue.png";
-import purp from "./purp.png";
-import black from "./black.png";
-import green from "./green.png";
+import React from "react"
+import { Card, CardGrid, Container, Header } from "./Elements"
 
+import "./App.css"
+
+import Menu from "./Menu"
+import blue from "./blue.png"
+import purp from "./purp.png"
+import black from "./black.png"
+import green from "./green.png"
+
+import Nav from "./components/Nav"
 import Inc from './components/Inc'
 import Mount from './components/Mount'
 import Hover from './components/Hover'
 import Toggle from './components/Toggle'
+import PageWrapper from './components/PageWrapper'
 
 function App() {
 	return (
-		<div className="App">
-			<Header>
-				<Menu />
-				<h1>Header</h1>
-			</Header>
-			<Container>
-				<h2>Super Cool</h2>
+		<PageWrapper>
+			<div className="App">
+				<Header>
+					<Menu />
+					<h1>Header</h1>
+				</Header>
+				<Nav />
+				<Container>
+					<h2>Super Cool</h2>
 
-				<Toggle />
-				<Inc />
-				<Mount />
-				<Hover />
+					<Toggle />
+					<Inc />
+					<Mount />
+					<Hover />
 
-				<CardGrid>
-					<Card style={{ background: "var(--purp)" }}>
-						<h3>Some card</h3>
-						<img src={purp} alt="purp" />
-					</Card>
-					<Card style={{ background: "var(--blue)" }}>
-						<h3>Some card</h3>
-						<img src={blue} alt="blue" />
-					</Card>
-					<Card style={{ background: "var(--black)" }}>
-						<h3>Some card</h3>
-						<img src={black} alt="black" />
-					</Card>
-					<Card style={{ background: "var(--green)" }}>
-						<h3>Some card</h3>
-						<img src={green} alt="green" />
-					</Card>
-				</CardGrid>
-			</Container>
-		</div>
-	);
+					<CardGrid>
+						<Card style={{ background: "var(--purp)" }}>
+							<h3>Some card</h3>
+							<img src={purp} alt="purp" />
+						</Card>
+						<Card style={{ background: "var(--blue)" }}>
+							<h3>Some card</h3>
+							<img src={blue} alt="blue" />
+						</Card>
+						<Card style={{ background: "var(--black)" }}>
+							<h3>Some card</h3>
+							<img src={black} alt="black" />
+						</Card>
+						<Card style={{ background: "var(--green)" }}>
+							<h3>Some card</h3>
+							<img src={green} alt="green" />
+						</Card>
+					</CardGrid>
+				</Container>
+			</div>
+		</PageWrapper>
+	)
 }
 
-export default App;
+export default App
